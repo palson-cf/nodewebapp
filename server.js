846c625006f5f856990b54b09773bce209bea7ad
jagcf <jag@codefresh.io>
2019-12-30 17:25:11 -0800

    new change

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate demo application!');
+  response.end('Hello from Node Server App deployed on ECS Fargate demo application!. Test2');
   
 };
 var www = http.createServer(handleRequest);
 www.listen(80,function(){
 
-    console.log("server listnening on 8080");
+    console.log("server listnening on 80");
+    console.log("server listnening on 80");
 });
